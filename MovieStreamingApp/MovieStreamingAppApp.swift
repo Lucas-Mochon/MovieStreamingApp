5e391d6ad3c69a96db041ba59826944668d829cd
Lucas-Mochon <mochonlucas@gmail.com>
2026-02-04 09:09:47 +0100

    Initial Commit

---
+++ MovieStreamingApp/MovieStreamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStreamingAppApp.swift
+//  MovieStreamingApp
+//
+//  Created by Cours on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieStreamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
